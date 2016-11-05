8aa55fa20a19fd7d1acd274191ada72e213aabc4
김성찬 <dlrj1234@naver.com>
2016-11-05 22:15:25 +0900

    OnlyNumber 완성

--- homework6/src/homework6/OnlyNumber.java
+++ homework6/src/homework6/OnlyNumber.java
@@ -1,7 +1,26 @@
 package homework6;
-
+import java.util.*;
 public class OnlyNumber {
 	public void OnlyNumberMethod(){
+		   Scanner s = new Scanner(System.in);
 		   
+		   int arr[] = new int[10];
+		   int check[] = new int[10];
+		   System.out.println("1~100 사이의 숫자를 입력하세요.");
+		   for(int i = 0 ; i < 10 ; i++){
+			   System.out.print(i+1+"번째 숫자 :");
+			   arr[i] = s.nextInt();
+			   check[i] = arr[i];
+			   
+			   for(int j = 0 ; j < i ; j++){
+				   if(check[i] == arr[j]){
+					   i-=1;
+					   System.out.println("잘못 입력하셨습니다. 다시 입력하세요");
+				   }
+			   } 
+		   }
+		   for(int i = 9 ; i >= 0 ; i--){
+			   System.out.print(i+1+"번째 숫자는"+arr[i]+"입니다 \n");
+		   }
 	   }
 }
