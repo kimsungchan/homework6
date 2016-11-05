79e785593f2c8745d23fb607c99bdbbe9d57b797
김성찬 <dlrj1234@naver.com>
2016-11-05 21:18:32 +0900

    StudentScore 완료

--- homework6/src/homework6/StudentScore.java
+++ homework6/src/homework6/StudentScore.java
@@ -4,31 +4,53 @@ import java.text.DecimalFormat;
 public class StudentScore {
 	 public void StudentScoreMethod(){
 		 Scanner s = new Scanner(System.in);
-		 
-		 String pattern = "##.#";
-	     DecimalFormat dformat = new DecimalFormat(pattern);
+		
+	     double student_arr[][] = new double[5][3];
+	     
+	     for(int i = 0 ; i < 5 ; i++){
+	    	 System.out.print(i+1+"번 학생 국어, 영어, 수학 :");
+	    	 for(int j = 0 ; j < 3 ; j++){
+	    		 student_arr[i][j] = s.nextInt();
+	    	 }
+	     }   
+	     subject_sum(student_arr);
+	     student_sum(student_arr);
+	 }
+	 public void subject_sum(double student_arr[][]){
+		 String pattern = "##.##";
+		 DecimalFormat dformat = new DecimalFormat(pattern);
 	      
-		 int[][] student_arr = new int[5][4];
-	     int[] student_sum = {};
-		 
-		 for(int i = 0 ; i < 5 ; i++){
-			 System.out.println(i+1+"번 학생 국어 영어 수학 :");
-			 for(int j = 0 ; j < 3 ; j++){
-				 student_arr[i][j] = s.nextInt();
-				 student_arr[i][3] = student_arr[i][3] + student_arr[i][j];
-			 }
-		 }
-		 
-		 for(int i = 0 ; i < 3 ; i++){
-			 for(int j = 0 ; j < 5 ; j++){
-				 student_sum[i] = student_sum[i] + student_arr[j][i];
-			 }
-		 }
-		 System.out.print("국어 총점은" +student_sum[0]+"이고 평균은"+dformat.format(student_sum[0]/5.0) +"입니다.\n");
-		 System.out.print("영어 총점은" +student_sum[1]+"이고 평균은"+dformat.format(student_sum[1]/5.0) +"입니다.\n");
-		 System.out.print("수학 총점은" +student_sum[2]+"이고 평균은"+dformat.format(student_sum[2]/5.0) +"입니다.\n");
-         for(int i = 0 ; i < 5 ; i++){
-		 System.out.print(i+1+"번 학생의 총점은" +student_arr[i][3]+"이고 평균은"+dformat.format(student_arr[i][3]/3.0)+"입니다.\n");
-         }
+		 String sub_name[] = {"국어", "영어", "수학"};
+		 double s_sum[] = new double[3];
+	     double s_avg[] = new double[3];
+	     
+	     for(int i = 0 ; i < 3 ; i++){
+	    	 for(int j = 0 ; j < 5 ; j++){
+	    		 s_sum[i] += student_arr[j][i];
+	    	 }
+	    	 s_avg[i] = s_sum[i] / 5.0;
+	     }
+	     for(int h = 0 ; h < 3 ; h++){
+			System.out.print(sub_name[h]+"총점은"+s_sum[h]+"이고 평균은"+dformat.format(s_avg[h])+"입니다.\n");
+	     }
+	 } 
+	 
+	 public void student_sum(double student_arr[][]){
+		 String pattern = "##.##";
+		 DecimalFormat dformat = new DecimalFormat(pattern);
+	      
+		 double s_sum[] = new double[5];
+	     double s_avg[] = new double[5];
+	     
+	     for(int i = 0 ; i < 5 ; i++){
+	    	 for(int j = 0 ; j < 3 ; j++){
+	    		 s_sum[i] += student_arr[i][j];
+	    	 }
+	    	 s_avg[i] = s_sum[i] / 3.0;
+	     }
+	     for(int h = 0 ; h < 5 ; h++){
+	    	 System.out.print(h+1+"번 학생의 총점은" +s_sum[h]+"평균은"+dformat.format(s_avg[h])+"이다.\n");
+	     }
 	 }
 }
+
